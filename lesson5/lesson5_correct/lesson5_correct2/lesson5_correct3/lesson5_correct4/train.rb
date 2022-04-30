30895483a7e0f2b1e3853b688c53db8e86202076
koleshchatov <101318300+koleshchatov@users.noreply.github.com>
2022-04-30 16:30:25 +0500

    Add files via upload

---
+++ lesson5/lesson5_correct/lesson5_correct2/lesson5_correct3/lesson5_correct4/train.rb
@@ -0,0 +1,105 @@
+require_relative 'module_company'
+require_relative 'module_instance_counter'
+
+class Train
+  include Company
+  include InstanceCounter
+
+  attr_reader :wagon, :number, :type
+  
+  def self.show
+    @@all_train
+  end
+
+  @@all_train = []
+
+  def self.find(number)
+    @@all_train.find { |train| train.number == number }
+  end
+
+  def initialize(number, type)
+    @speed = 0
+    @number = number
+    @type = type
+    @wagon = []
+    @@all_train << self
+    register_instance 
+  end
+
+  def go_speed(speed) 
+    @speed += speed
+  end
+
+  def stop_speed(speed)
+    @stop_speed -= speed
+    if @speed < 0
+      @speed = 0
+    end
+  end
+
+  def plus_wagon(wagon)
+    if @speed == 0 && @type == wagon.type
+      @wagon << wagon
+    end
+  end
+
+  def minus_wagon(wagon)
+    if @speed == 0
+      @wagon.delete_at(wagon)
+    end
+  end
+
+  # def plus_wagon
+  #   if @speed == 0 
+  #     @wagon += 1
+  #   else
+  #     puts "Stop train"
+  #   end
+  # end
+
+  # def minus_wagon
+  #   if speed == 0
+  #     @wagon -= 1
+  #   else
+  #     puts "Stop train"
+  #   end
+  # end
+
+  def train_route(route)
+    @route = route
+    @index = 0
+    @route.stations[@index].add_train(self)
+    @route_train << route
+  end
+
+  def forward_train
+    @route.stations[@index].start(self)
+    @index += 1
+    @route.stations[@index].add_train(self)
+  end
+
+  def back_train
+    @route.stations[@index].start(self)
+    @index -= 1
+    @route.stations[@index].add_train(self)     
+  end
+
+  def next_station
+    @route.stations[@index + 1]
+  end
+
+  def previous_station
+    if @index > 0
+      @route.stations[@index - 1]
+    end
+  end
+
+#перенес этот метод в секцию  protected , так как по заданию нужно было перенести какой нибудь метод
+#Теперь , в моей программе, на объектах класса Train(Passenger/Cargo) нельзя вызвать метод current_station
+#чтобы узнать текущую станцию. 
+  protected
+
+  def current_station
+    @route.station[@index]
+  end
+end
