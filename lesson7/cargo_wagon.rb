a55f165abe288275582c890a3855375247475db5
koleshchatov <101318300+koleshchatov@users.noreply.github.com>
2022-05-08 10:18:05 +0500

    Add files via upload

---
+++ lesson7/cargo_wagon.rb
@@ -0,0 +1,23 @@
+class CargoWagon < Wagon
+  def initialize(type = :cargo, all_volume)
+    @all_volume = all_volume
+    @occupited_volume = 0
+    super(type)
+  end
+
+  def occupited_volume(occupited_volume)
+    if @occupited_volume + occupited_volume < @all_volume
+      @occupited_volume += occupited_volume
+    else
+      puts "В этом вагоне не хватает объема"
+    end  
+  end
+
+  def all_occupited_volume
+    @occupited_volume
+  end
+
+  def free_volume
+    all_volume - @occupited_volume
+  end  
+end
---
+++ lesson7/route.rb
@@ -0,0 +1,20 @@
+class Route
+  attr_reader :stations, :intermediate_stations, :starting_station, :end_station
+
+  def initialize(starting_station, end_station)
+    @starting_station = starting_station
+    @end_station = end_station
+    @intermediate_stations = []
+    @stations = [@starting_station, @end_station]
+  end
+
+  def add_station(name)
+    @stations.insert(-2, name)
+    @intermediate_stations << name
+  end
+
+  def delete_stations(name)
+    @stations.delete_at(name + 1)
+    @intermediate_stations.delete_at(name)
+  end
+end
